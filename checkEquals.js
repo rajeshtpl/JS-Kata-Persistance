79e43e02bf83a49c65ae328575c652bc35eb421c
unknown <rajeshtpl@gmail.com>
2018-01-17 19:49:34 -0600

    added code for the persistance using reduce , split

--- checkEquals.js
+++ checkEquals.js
@@ -1,8 +1,34 @@
 
 
-const cityname ='desmoines'
-var checkEquals = input => input === cityname
+// const cityname ='desmoines'
+// var checkEquals = input => input === cityname
 
-export {
-    checkEquals
-}
+// export {
+//     checkEquals
+// }
+
+
+
+
+function persistance(number){
+    var arrayDigits = number.toString().split('')
+    console.log(arrayDigits)
+    console.log(arrayDigits.length)
+    if(arrayDigits.length == 1){
+        console.log('digit length is 1')
+    }else{
+      var multiplyValue = arrayDigits.reduce(function(product,element){
+        console.log(product)  
+        return parseInt(product) * parseInt(element)
+      },1)
+      console.log(multiplyValue)
+      }
+    }
+    
+    persistance(66)
+    
+    
+    
+    
+    
+    
