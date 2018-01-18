ea82d54cc33422562f2af3e8a65605278b045f15
rajeshtpl <rajeshtpl@gmail.com>
2018-01-18 12:39:49 -0600

    added a create phone number function

---
+++ phoneNumber.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers){
+  var formattedNumber = '(' + numbers[0]  + numbers[1] + numbers[2] +') ' 
+                        + numbers[3]  + numbers[4] + numbers[5] + '-'
+                        + numbers[6]  + numbers[7] + numbers[8] + numbers[9] 
+
+  return formattedNumber
+}
